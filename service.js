d3de87f7b33e05a53ca8301ba98b0650e6d9c9a6
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-31 22:06:40 +0900

    Add files via upload

--- service.js
+++ service.js
@@ -8,7 +8,26 @@ $(function() {
     $('#tsuma').fadeIn();
   });
 
+  // ここからメニュー
+  $('#bar').click(function(){
+    $('#menu').slideDown();
+  });
 
+  $('#bar').click(function(){
+    $(this).fadeOut();
+  });
+
+  $('#batsu').click(function(){
+    $('#menu').slideUp();
+  });
+
+
+
+  $('#batsu').click(function(){
+    $('#bar').fadeIn();
+  });
+
+// ここまでメニュー
 
 
 
